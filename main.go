2798596cee9344010624b8500f13cf12e6c1301c
Guney Can Gokoglu <guneycan@gmail.com>
2015-01-21 21:14:59 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	redisHost = ":6379"
+)
+
+func main() {
+
+	fmt.Println("Connecting to redis...")
+
+	c, err := redis.Dial("tcp", redisHost)
+	defer c.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Getting all the keys from redis...")
+
+	keys, err := redis.Values(c.Do("KEYS", "*"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	type Key struct {
+		r int
+		m int64
+	}
+
+	stats := make(map[string]*Key)
+
+	for _, key := range keys {
+		var keyValue, ok = key.([]byte)
+		if ok {
+			parts := strings.Split(string(keyValue), ":")
+			keyName := parts[0]
+
+			_, ok := stats[keyName]
+
+			if ok == false {
+				e := &Key{
+					r: 0,
+					m: 0,
+				}
+				stats[keyName] = e
+			}
+
+			i := stats[keyName].r
+			i += 1
+
+			stats[keyName].r = i
+
+			d, err := redis.String(c.Do("DEBUG", "OBJECT", string(keyValue)))
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			r, _ := regexp.Compile("serializedlength:(.*?) ")
+			text := r.FindString(d)
+
+			mem := strings.Trim(strings.Split(text, ":")[1], " ")
+			memNumber, err := strconv.ParseInt(mem, 0, 64)
+
+			if err == nil {
+				m := stats[keyName].m
+				m += memNumber
+				stats[keyName].m = m
+			}
+
+		}
+	}
+
+	fmt.Println("All the keys processed")
+
+	fmt.Println("key name", ",", "repetition", ",", "used memory")
+	for keyName, values := range stats {
+		fmt.Println(keyName, ",", values.r, ",", values.m/1000000)
+	}
+
+	fmt.Println("EOF")
+
+}
